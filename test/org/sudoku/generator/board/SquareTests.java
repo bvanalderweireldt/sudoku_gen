8af5749cfa497e75665bf480e8e3eb2ba13f9823
Benoit Vanalderweireldt <benoit.vanalderweireldt@gmail.com>
2015-03-31 18:13:34 +0700

    add new test

--- test/org/sudoku/generator/board/SquareTests.java
+++ test/org/sudoku/generator/board/SquareTests.java
@@ -6,8 +6,18 @@ import org.junit.Test;
 
 public class SquareTests {
 	
+	Square validSquare = new Square();
 	@Before
 	public void init(){
+		validSquare.addCell(0, 0, new Cell(5));
+		validSquare.addCell(0, 1, new Cell(4));
+		validSquare.addCell(0, 2, new Cell(8));
+		validSquare.addCell(1, 0, new Cell(2));
+		validSquare.addCell(1, 1, new Cell(3));
+		validSquare.addCell(1, 2, new Cell(9));
+		validSquare.addCell(2, 0, new Cell(1));
+		validSquare.addCell(2, 1, new Cell(7));
+		validSquare.addCell(2, 2, new Cell(6));
 	}
 
 	@Test
@@ -17,6 +27,11 @@ public class SquareTests {
 		square.addCell(0, 1, new Cell(4));
 		
 		assertFalse(square.squareValid());
+	}
+	
+	@Test
+	public void squareValid(){
+		assertTrue(validSquare.squareValid());
 	}
 	
 	@Test
